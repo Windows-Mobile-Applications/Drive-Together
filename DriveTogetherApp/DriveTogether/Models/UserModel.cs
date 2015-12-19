6da0d50dbe46831bb1aad9fcb619b6fd293c23a2
zdzdz <zdzdz.link@yahoo.com>
2015-12-19 18:17:36 +0200

    Commited new DB model

---
+++ DriveTogetherApp/DriveTogether/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿namespace DriveTogether.Models
+{
+    using SQLite.Net.Attributes;
+
+    public class UserModel
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public string Password { get; set; }
+    }
+}

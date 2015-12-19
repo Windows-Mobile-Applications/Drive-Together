89ba38f3a450841cee6fd2d15ab09cddf91340d2
zdzdz <zdzdz.link@yahoo.com>
2015-12-19 10:41:53 +0200

    Added test LogOut button on Homepage.

---
+++ DriveTogetherApp/DriveTogether/Models/SaveStateModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DriveTogether.Models
+{
+    using SQLite.Net.Attributes;
+
+    public class SaveStateModel
+    {
+        [PrimaryKey]
+        [AutoIncrement]
+        public int Id { get; set; }
+
+        public bool IsSignedIn { get; set; }
+    }
+}
---
+++ DriveTogetherApp/DriveTogether/Pages/HomePage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace DriveTogether.Pages
+{
+    using System;
+    using System.IO;
+    using Windows.Storage;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    
+    using SQLite.Net;
+    using SQLite.Net.Async;
+    using SQLite.Net.Platform.WinRT;
+
+    using Models;
+
+    public sealed partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void OnLogOutButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.DropTable();
+            this.Frame.Navigate(typeof(MainPage));
+        }
+
+        private SQLiteAsyncConnection GetDbConnectionAsync()
+        {
+            var dbFilePath = Path.Combine(ApplicationData.Current.LocalFolder.Path, "db.sqlite");
+
+            var connectionFactory =
+                new Func<SQLiteConnectionWithLock>(
+                    () =>
+                    new SQLiteConnectionWithLock(
+                        new SQLitePlatformWinRT(),
+                        new SQLiteConnectionString(dbFilePath, storeDateTimeAsTicks: false)));
+
+            var asyncConnection = new SQLiteAsyncConnection(connectionFactory);
+
+            return asyncConnection;
+        }
+
+        private async void DropTable()
+        {
+            var connection = this.GetDbConnectionAsync();
+            await connection.DeleteAllAsync<SaveStateModel>();
+        }
+    }
+}
